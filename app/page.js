4c1c7c5fbc69ce09c4fee1eca4c0304ac15e98e4
Carlos Villegas <darth.qach@gmail.com>
2024-04-07 13:40:22 -0600

    Create hero section

--- app/page.js
+++ app/page.js
@@ -1,9 +1,11 @@
 import NavBar from "@/components/NavBar";
+import HeroSection from "@/components/main/HeroSection";
 
 export default function Home() {
   return (
     <>
       <NavBar />
+      <HeroSection />
     </>
   );
 }
--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
+// next.config.mjs
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  output: 'export',
+  // Include any other configuration you might have...
+};
 
 export default nextConfig;
